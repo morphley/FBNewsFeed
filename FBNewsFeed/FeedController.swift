b5336720d48a2ff9a43059b3a2f14ba0161998f7
morphley <developer.omari@gmail.com>
2017-03-20 19:57:30 +0100

    Removed storyboardFiles Removed Main from Main Interface (App settings) Create our window and make it visible black Screen Create a NavigationControllerBar on the top filled with the feedController with a a black collectionView (Default Color Black)

--- FBNewsFeed/FeedController.swift
+++ FBNewsFeed/FeedController.swift
@@ -8,17 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.blue
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+    
+    
 
 
 }
