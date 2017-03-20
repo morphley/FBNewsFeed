c3718dcc6f302db452c598abc9c14db8739d36ca
morphley <developer.omari@gmail.com>
2017-03-20 20:57:55 +0100

    Create a couple of cells which are not fully expanded

--- FBNewsFeed/FeedController.swift
+++ FBNewsFeed/FeedController.swift
@@ -11,26 +11,48 @@ import UIKit
 let cellId = "cellId"
 
 class FeedController: UICollectionViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         navigationItem.title = "Facebook Feed"
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1) // make it gray
         
-       // collectionView?.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellWithReuseIdentifier: )
+        collectionView?.register(FeedCell.self, forCellWithReuseIdentifier: cellId ) //register the class
         
     }
     
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 3
-//    }
-//    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
     
-  
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        
+    }
     
     
-
-
 }
 
+class FeedCell: UICollectionViewCell{
+
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        setupViews()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setupViews(){
+        
+        backgroundColor = UIColor.white
+    
+    
+    }
+}
